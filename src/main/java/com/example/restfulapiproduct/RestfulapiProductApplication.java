236281520df34c99271f0bdad32869fb3bd999ed
kenpi13 <kenkenpi0526@gmail.com>
2020-12-12 23:38:26 +0900

    initial commit

---
+++ src/main/java/com/example/restfulapiproduct/RestfulapiProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulapiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulapiproduct/RestfulapiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
